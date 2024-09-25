7ced59a54c5589b9257668aad962cab4d8a70837
bunyodruzimboyev <=>
2024-09-25 19:18:59 +0500

    Project is created

---
+++ src/main/java/uz/br29/appschool/AppSchoolApplication.java
@@ -0,0 +1,13 @@
+package uz.br29.appschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSchoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppSchoolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/br29/appschool/AppSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.br29.appschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppSchoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
